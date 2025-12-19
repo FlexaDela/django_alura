551a52e3cecc7447158f29f5e8c1359820fd11cb
FlexaDela <mateusflexa2000@gmail.com>
2025-12-19 20:35:48 -0300

    adicionando filtros no admin e status choices

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Fotografia
 
-# Register your models here.
+
+@admin.register(Fotografia)
+class FotografiaAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "legenda") # vai mostrar estes campos
+    list_display_links = ("id", "name") # vai adicionar um link nestes campos
+    search_fields = ("name",) #ele precisa ser uma tupla e adiciona a aba de buscar pelo nome
+    list_filter = ("categoria")
+
--- galeria/models.py
+++ galeria/models.py
@@ -3,8 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+
+    CATEGORY_CHOICES = (
+        ("NEBULOSA", "nebulosa"),
+        ("ESTRELA","estrela"),
+        ("GALAXIA","galaxia"),
+        ("PLANETA","planeta")
+    )
+
+
     name = models.CharField(max_length=100, null = False, blank=False)
     legenda = models.CharField(max_length=100, null = False, blank=False)
+    categoria = models.CharField(max_length=100, default='', choices=CATEGORY_CHOICES)
     descricao = models.TextField(null = False, blank=False)
     foto = models.CharField(max_length=100, null = False, blank=False)
 
