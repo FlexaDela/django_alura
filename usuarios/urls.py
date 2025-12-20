f3ea217b00aa710579a3ea1df66de74e878090f9
FlexaDela <mateusflexa2000@gmail.com>
2025-12-20 18:05:49 -0300

    adicionando html de login e cadastro views e rotas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-
+from .views import login, cadastro, logout
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-   #path(),
+   path('login/', login, name='login'),
+   path('cadastro/', cadastro, name='cadastro'),
+   path('logout/', logout, name='logout')
 ]
 
 if settings.DEBUG:
