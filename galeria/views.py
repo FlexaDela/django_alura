a79964bfa54f2abba2a737a1594b91ef83194ea3
FlexaDela <mateusflexa2000@gmail.com>
2025-12-20 14:45:13 -0300

    adicionando filtros

--- galeria/views.py
+++ galeria/views.py
@@ -12,3 +12,10 @@ def imagem(request, id):
     foto = get_object_or_404(Fotografia, id=id)
 
     return render(request, 'galeria/image.html', {"foto":foto})
+
+def buscar(request):
+    fotos = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
+    nome_buscar = request.GET['buscar']
+    if nome_buscar:
+       filtrada = fotos.filter(name_icontains = nome_buscar)
+    return render(request, "galeria/buscar.html",{"fotografias":filtrada})
