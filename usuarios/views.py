785f578c5a3399d730b9851bac789cdb0a67eec3
FlexaDela <mateusflexa2000@gmail.com>
2025-12-23 23:59:08 -0300

    fazendo o form

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from usuarios.forms import LoginForms, CadastroForms
 
 def login(request):
@@ -7,5 +7,12 @@ def login(request):
     return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    form = CadastroForms()
+    form = CadastroForms(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                return redirect('cadastro')
+            nome = form['']
+            form.save()
+
     return render(request, 'usuarios/cadastro.html')
